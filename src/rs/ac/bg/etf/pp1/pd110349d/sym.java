846e514ee276de727f48647352f14ebbaff3ad38
cog26083 <dsnpetrovic2@gmail.com>
2016-02-08 16:45:22 +0100

    Added sym class

---
+++ src/rs/ac/bg/etf/pp1/pd110349d/sym.java
@@ -0,0 +1,54 @@
+package rs.ac.bg.etf.pp1.pd110349d;
+
+public class sym {
+	
+	//Keywords
+	public static final int PROG 	= 1;
+	public static final int BREAK 	= 2;
+	public static final int CLASS 	= 3;
+	public static final int ELSE 	= 4;
+	public static final int CONST 	= 5;
+	public static final int IF 		= 6;
+	public static final int NEW		= 7;
+	public static final int PRINT 	= 8;
+	public static final int READ 	= 9;
+	public static final int RETURN	= 10;
+	public static final int VOID	= 11;
+	public static final int WHILE 	= 12;
+	public static final int EXTENST	= 13;
+	
+	//Token types
+	public static final int IDENT	= 14;
+	public static final int NUM 	= 15;
+	public static final int CHAR_CONST	= 16;
+	public static final int BOOL_CONST	= 17;
+	public static final int STR_CONST	= 18;
+
+	//Operators
+	public static final int PLUS	= 19; // +
+	public static final int MINUS	= 20; // -
+	public static final int MUL		= 21; // *
+	public static final int DIV		= 22; // /
+	public static final int MOD		= 23; // %
+	public static final int EQUAL	= 24; // ==
+	public static final int N_EQUAL	= 25; // !=
+	public static final int GR_THAN	= 26; // >
+	public static final int GR_EQ_THAN	= 27; // >=
+	public static final int LE_THAN	= 28; // <
+	public static final int LE_EQ_THAN	= 29; // <=
+	public static final int LOG_AND	= 30; // &&
+	public static final int LOG_OR	= 31; // ||
+	public static final int ASSIGN 	= 32; // =
+	public static final int INC		= 33; // ++
+	public static final int DEC		= 34; // --
+	public static final int SEMI	= 35; // ;
+	public static final int COMMA	= 36; // ,
+	public static final int DOT		= 37; // .
+	public static final int L_PAREN	= 38; // (
+	public static final int R_PAREN	= 39; // )
+	public static final int L_BRACKET	= 40; // [
+	public static final int R_BRACKET	= 41; // ]
+	public static final int L_CURLY	= 42; // {
+	public static final int R_CURLY	= 43; // }	
+	
+}
